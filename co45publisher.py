1625be2b70fa4fa5342c25a3dbadc30215fbc9ce
muhammed-naji-ac <muhammadnajiac164@gmail.com>
2025-11-27 10:52:54 +0530

    python 27/11/25

---
+++ co45publisher.py
@@ -0,0 +1,37 @@
+class Publisher:
+    def __init__(self, name):
+        print("Publisher class activated")
+        self.name = name
+
+    def display(self):
+        print(f"Name: {self.name}")
+
+
+class Book(Publisher):
+    def __init__(self, name, title, author):
+        super().__init__(name)
+        print("Book class activated")
+        self.title = title
+        self.author = author
+
+    def display(self):
+        super().display()
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+
+
+class Python(Book):
+    def __init__(self, name, title, author, price, no_pages):
+        super().__init__(name, title, author)
+        print("Python class activated")
+        self.price = price
+        self.no_pages = no_pages   # fixed syntax
+
+    def display(self):
+        super().display()
+        print(f"Price: {self.price}")
+        print(f"No of pages: {self.no_pages}")
+
+
+book1 = Python("DC", "Aadujeevitham", "Basheer", 540, 250)
+book1.display()
